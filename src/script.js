075f99c178d6332c173d3c9a6957bcdbe4abdc60
MOrtmann90 <mauro.ortmann@gmail.com>
2023-06-04 18:06:33 -0300

    mobile version

---
+++ src/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    dots: true, // Muestra los indicadores de navegación
+    infinite: true, // Permite la navegación continua
+    speed: 500, // Velocidad de transición en milisegundos
+    autoplay: true,
+    autoplaySpeed: 2500,
+    slidesToShow: 3, // Número de imágenes que se muestran a la vez
+    slidesToScroll: 1, // Número de imágenes que se desplazan al avanzar o retroceder
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
