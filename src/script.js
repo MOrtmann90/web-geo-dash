dbe539ef0bb6768a3c6016270d7cdff9b9035f31
MOrtmann90 <mauro.ortmann@gmail.com>
2023-06-04 23:14:35 -0300

    v1.0

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".carousel").slick({
+  $(".sliders").slick({
     dots: true, // Muestra los indicadores de navegación
     infinite: true, // Permite la navegación continua
     speed: 500, // Velocidad de transición en milisegundos
@@ -9,7 +9,7 @@ $(document).ready(function () {
     slidesToScroll: 1, // Número de imágenes que se desplazan al avanzar o retroceder
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1366,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
@@ -18,14 +18,14 @@ $(document).ready(function () {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 767,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
